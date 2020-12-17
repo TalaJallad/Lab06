153bd6fdb89c2a30961e2ca9b4af18e80047faad
Tala Jallad <talaj2911@gmail.com>
2020-12-17 13:12:47 +0200

    JavaScript file

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var username = prompt("What is your name?");
+
+alert= ("Hello " + username + "! " + "Welcme to Brown Paper Company");
+
+
+var action = prompt("What do you want to do? Options: 1. Contact us 2. Learn about us 3.Shop (Please write down the number of one option");
+
+if (action == 1) {
+    alert ("Phone: 0798321274, Email: Bpaperco@bpc.net");}
+
+else if (action == 2) {
+    alert ("Our company was founded in 1998 and has grown to become the larget in the east cost with th highest quality paper");}
+    
+else if (action == 3) {
+    var papertype = prompt ("You will send you an email with all price information! Please enter your email below.");
+alert("Thank you"); }
+
+else {alert ("Invalid Entry");}
+       
+            
+    
+    
+
+
+
